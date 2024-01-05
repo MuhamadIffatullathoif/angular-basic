a00ed2e4ddebcba2dfecfb7202bf7a641e5b65b2
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-05 19:38:24 +0700

    05. Counter Functionality

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,8 +2,21 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-counter',
-  template: `<h3>Counter</h3>`
+  template: `
+    <h3>Count: {{count}}</h3>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `
 })
 export class CounterComponent {
+  public count: number = 10;
 
+  increaseBy(value: number): void {
+    this.count += value;
+  }
+
+  resetCounter(): void {
+    this.count = 10;
+  }
 }
